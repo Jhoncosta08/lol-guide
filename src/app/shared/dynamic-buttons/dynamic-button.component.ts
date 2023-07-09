8f6eb52da77d7e5aa5ae7ba3601f01b64491eb3d
jhonatan <jhonatan.s.costa97@gmail.com>
2023-07-09 17:53:39 -0300

    feat(app): add dynamic buttons code

--- src/app/shared/dynamic-buttons/dynamic-button.component.ts
+++ src/app/shared/dynamic-buttons/dynamic-button.component.ts
@@ -11,5 +11,7 @@ export class DynamicButtonComponent {
   @Input({required: true}) buttonStyle: 'primary' | 'outline' | 'warn' | 'danger' = 'primary';
   @Input({required: true}) buttonType: 'button' | 'submit' | 'reset' = 'button';
   @Input({required: true}) buttonDisable: boolean = false;
+  @Input() buttonWidth: number = 150;
+  @Input() buttonHeight: number = 35;
   @Output() buttonAction: EventEmitter<void> = new EventEmitter<void>();
 }
