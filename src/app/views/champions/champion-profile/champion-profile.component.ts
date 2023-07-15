6fe2041bac06f4a39e84c11decb32e0f7db9c1db
jhonatan <jhonatan.s.costa97@gmail.com>
2023-07-15 00:53:03 -0300

    feat(app): champion profile add champion lane and difficulty

--- src/app/views/champions/champion-profile/champion-profile.component.ts
+++ src/app/views/champions/champion-profile/champion-profile.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ChampionProfileComponent implements OnInit{
   champData: any;
   showSpinner: boolean = true;
+  championLanes: string[] = [];
   constructor(
     private championsService: ChampionsService,
     readonly activatedRoute: ActivatedRoute
@@ -25,12 +26,47 @@ export class ChampionProfileComponent implements OnInit{
   getProfileChamp(championParamsId: string) {
     this.championsService.getChampionById(championParamsId).then(res => {
       this.champData = res.data();
+      this.getChampionLanes();
       this.showSpinner = false;
       console.log('champ: ', this.champData);
     }).catch(err => {
       this.showSpinner = false;
       console.error('Error when tried to get champ by id', err);
     });
+  }
+
+  getChampionLanes() {
+    const champDataLane = this.champData?.championLanes;
+    const championLanes = this.championLanes;
+
+    const laneCategories = ['top', 'mage', 'adc', 'jungle', 'support'];
+    for (const lane of laneCategories) {
+      if (champDataLane[lane] && !championLanes.includes(champDataLane[lane])) {
+        championLanes.push(champDataLane[lane]);
+      }
+    }
+  }
+
+  checkIconLane(lane: string): string {
+    let iconLane: string = 'assets/icons/lane-icons';
+    switch (lane) {
+      case "Topo":
+        iconLane = `${iconLane}/top.png`;
+        break;
+      case "Meio":
+        iconLane = `${iconLane}/mid.png`;
+        break;
+      case "Adc":
+        iconLane = `${iconLane}/bot.png`;
+        break;
+      case "Selva":
+        iconLane = `${iconLane}/jungle.png`;
+        break;
+      case "Support":
+        iconLane = `${iconLane}/support.png`;
+        break;
+    }
+    return iconLane;
   }
 
 
