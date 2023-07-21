9156d782ff3f22a5d42be287a69adbd640c6c70a
jhonatan <jhonatan.s.costa97@gmail.com>
2023-07-21 16:55:57 -0300

    feat(app): remove unused code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
-export const environment = {
+export const environment  = {
+  production: false,
+  apiUrl: 'projects/lol-guide-web/databases/(default)/documents',
   firebase: {
-    projectId: 'lol-guide-web',
-    appId: '1:947637903349:web:e3688dc91900062409cabc',
-    databaseURL: 'https://lol-guide-web-default-rtdb.firebaseio.com',
-    storageBucket: 'lol-guide-web.appspot.com',
-    apiKey: 'AIzaSyC6dpjsuf-E84ZGbrirfQVa4miNAKJSffw',
-    authDomain: 'lol-guide-web.firebaseapp.com',
-    messagingSenderId: '947637903349',
-  },
-};
+    apiKey: "AIzaSyC6dpjsuf-E84ZGbrirfQVa4miNAKJSffw",
+    authDomain: "lol-guide-web.firebaseapp.com",
+    databaseURL: "https://lol-guide-web-default-rtdb.firebaseio.com",
+    projectId: "lol-guide-web",
+    storageBucket: "lol-guide-web.appspot.com",
+    messagingSenderId: "947637903349",
+    appId: "1:947637903349:web:e3688dc91900062409cabc",
+  }
+}
